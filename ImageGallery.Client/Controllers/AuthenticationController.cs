db8afba2b8bfda8c61f4841dd93a370be835e054
Todd Hilehoffer <toddhilehoffer@yahoo.com>
2024-06-23 17:52:42 -0400

    Logging out of  the web client application

---
+++ ImageGallery.Client/Controllers/AuthenticationController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageGallery.Client.Controllers {
+    public class AuthenticationController : Controller {
+        [Authorize]
+        public async Task Logout() {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+        }
+    }
+}
