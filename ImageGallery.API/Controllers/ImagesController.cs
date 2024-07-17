c4580573c7900a5d317108ddb4a3932916c7976d
Todd Hilehoffer <toddhilehoffer@yahoo.com>
2024-07-17 13:21:06 -0400

    Protecting create image api

--- ImageGallery.API/Controllers/ImagesController.cs
+++ ImageGallery.API/Controllers/ImagesController.cs
@@ -73,6 +73,7 @@ namespace ImageGallery.API.Controllers {
             }
 
         [HttpPost()]
+        [Authorize(Roles = "PayingUser")]
         public async Task<ActionResult<Image>> CreateImage([FromBody] ImageForCreation imageForCreation) {
             // Automapper maps only the Title in our configuration
             var imageEntity = _mapper.Map<Entities.Image>(imageForCreation);
@@ -99,6 +100,12 @@ namespace ImageGallery.API.Controllers {
             // ownerId should be set - can't save image in starter solution, will
             // be fixed during the course
             //imageEntity.OwnerId = ...;
+            var ownerId = User.Claims.FirstOrDefault(c => c.Type == "sub")?.Value;
+            if (ownerId == null) {
+                throw new Exception("User Identity missing from token");
+                }
+            imageEntity.OwnerId = ownerId;  
+
 
             // add and save.  
             _galleryRepository.AddImage(imageEntity);
